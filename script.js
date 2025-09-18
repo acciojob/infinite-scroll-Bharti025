3513c1ebdbe84d973278ff10f07abf9542605a98
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-09-18 12:34:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 let lists=document.getElementById("infi-list");
 const listItems=lists.children;
 window.onscroll=()=>{
-	(lists.lastElementChild.getBoundingClientRect().bottom <= window.innerHeight){
+	(lists.lastElementChild.getBoundingClientRect().bottom <=
+	 window.innerHeight){
 document.createElement("li").append(listItems.length+1);
 document.createElement("li").append(listItems.length+1);
 	}
